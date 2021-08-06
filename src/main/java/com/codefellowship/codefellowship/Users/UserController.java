110b035d5e04bc07f76e0388f76f0cd46377dd3b
judyal-taweel <judybataineh@gmail.com>
2021-08-07 00:30:43 +0300

    MOJU lab18 & 19

--- src/main/java/com/codefellowship/codefellowship/Users/UserController.java
+++ src/main/java/com/codefellowship/codefellowship/Users/UserController.java
@@ -1,5 +1,4 @@
 package com.codefellowship.codefellowship.Users;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,15 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Optional;
-
 @Controller
 public class UserController {
-
-
     @Autowired
     UserRepository userRepository;
 
@@ -26,26 +21,22 @@ public class UserController {
     PostRepository postRepository;
 
     @Autowired
-   private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     @GetMapping("/")
     public String homePage(){
         return "home";
     }
-
     @GetMapping("/signup")
     public String getSignupPage(){
         return "signup";
     }
-
     @GetMapping("/login")
     public String getLoginPge(){
 //        model.addAttribute("userInfo",userRepository.findAll());
-
         return "logIn";
     }
-
-
     @GetMapping("/myprofile")
     public String getProfilePage(Model model){
         UserDetails userDetails= (UserDetails) SecurityContextHolder
@@ -55,13 +46,13 @@ public class UserController {
         Users users = userRepository.findUsersByUsername(userDetails.getUsername());
         model.addAttribute("posts",users.getPosts());
         model.addAttribute("username",userDetails.getUsername());
-
         model.addAttribute("userInfo",users);
+        userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Users userAccountLoggedIn = userRepository.findUsersByUsername(userDetails.getUsername());
+        model.addAttribute("noOfFollowers",userAccountLoggedIn.getFollowers().size());
+        model.addAttribute("noOfFollowing",userAccountLoggedIn.getFollowing().size());
         return "profile";
     }
-
-
-
     @PostMapping("/myprofile")
     public RedirectView createPost(@RequestParam String body){
         Date date = new Date();
@@ -74,15 +65,12 @@ public class UserController {
         post.setUsers(users);
         post.setCreatedAt(date);
         post= postRepository.save(post);
-
         return new RedirectView("/myprofile");
     }
-
-@GetMapping("/test")
-public String testTem(){
+    @GetMapping("/test")
+    public String testTem(){
         return "test";
-}
-
+    }
     @PostMapping("/signup")
     public RedirectView trySignUp(@RequestParam String firstname,
                                   @RequestParam String lastname,
@@ -93,25 +81,77 @@ public String testTem(){
                                   @RequestParam String bio){
         Users newUser = new Users(firstname,lastname,username,bCryptPasswordEncoder.encode(password),location,dateOfBirth,bio);
         newUser = userRepository.save(newUser);
-
         Authentication authentication= new UsernamePasswordAuthenticationToken(newUser,null,new ArrayList<>());
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
         return new RedirectView("/myprofile");
     }
-
-
-
     @RequestMapping(value="/users/{id}",method= RequestMethod.GET)
     public String getUsers(Model model, @PathVariable (value ="id") Long id){
         Optional<Users> userById=userRepository.findById(id);
         model.addAttribute("bios", userById);
         return "profile";
     }
-
     @GetMapping("/access-denied")
     public String getAccessDenied() {
         return "error";
     }
+
+
+
+
+    @GetMapping("/feeds")
+    public String getFeeds (Model model){
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Users userAccountLoggedIn = userRepository.findUsersByUsername(userDetails.getUsername());
+        model.addAttribute("followinguser",userAccountLoggedIn.getFollowing());
+        model.addAttribute("userName",userDetails.getUsername());
+        return "feeds";
+    }
+
+    @PostMapping("/following/{id}")
+    public RedirectView addFollowing(@PathVariable Long id) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Users userAccount = userRepository.findById(id).orElseThrow();
+        Users userAccountLoggedIn = userRepository.findUsersByUsername(userDetails.getUsername());
+        userAccountLoggedIn.addFollowing(userAccount);
+        userRepository.save(userAccountLoggedIn);
+        return new RedirectView("/user/{id}");
+    }
+    @GetMapping("/user/{id}")
+    public String getUsr(Model model , @PathVariable Long id)
+    {
+        model.addAttribute("users",userRepository.findById(id).orElseThrow());
+        return "userId";
+    }
+    @PostMapping("/unFollowing/{id}")
+    public RedirectView removeFollowing(@PathVariable Long id) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Users userAccount = userRepository.findById(id).orElseThrow();
+        Users userAccountLoggedIn = userRepository.findUsersByUsername(userDetails.getUsername());
+        userAccountLoggedIn.deleteFollowing(userAccount);
+        userRepository.save(userAccountLoggedIn);
+        return new RedirectView("/user/{id}");
+    }
+
+    //@GetMapping("/follow")
+//public String getFollowing(Model model){
+//    UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//    Users userAccountLoggedIn = userRepository.findUsersByUsername(userDetails.getUsername());
+//    model.addAttribute("noOfFollowers",userAccountLoggedIn.getFollowers().size());
+//    model.addAttribute("noOfFollowing",userAccountLoggedIn.getFollowing().size());
+//    return "follow";
+//
+//}
+    @GetMapping("/users")
+    public String getUsers(Model model)
+    {
+        model.addAttribute("users",userRepository.findAll());
+        return "userProfile";
+    }
+//    @GetMapping("/users/{id}")
+//    public String getUsersByPostss(Model model,@PathVariable Long id)
+//    {
+//        model.addAttribute("users",userRepository.findById(id).orElseThrow());
+//        return "userId";
+//    }
 }
--- src/main/java/com/codefellowship/codefellowship/Users/Users.java
+++ src/main/java/com/codefellowship/codefellowship/Users/Users.java
@@ -1,55 +1,66 @@
 package com.codefellowship.codefellowship.Users;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-
+import java.util.*;
 
 @Entity
 @Table(name="users")
 public class Users implements UserDetails {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-
     String firstname;
     String lastname;
     String dateOfBirth;
     String location;
     String bio;
-
     @OneToMany(mappedBy = "users")
     private List<Post> posts;
-
     public List<Post> getPosts() {
         return posts;
     }
 
+    @ManyToMany
+    @JoinTable(
+            name = "useraccount_useraccount",
+            joinColumns = {@JoinColumn(name = "from_id")},
+            inverseJoinColumns = {@JoinColumn(name = "to_id")}
+    )
+    public Set<Users> following= new HashSet<>();
+
+    public Set<Users> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Set<Users> following) {
+        this.following = following;
+    }
+
+    public List<Users> getFollowers() {
+        return followers;
+    }
+
+
+
+    @ManyToMany(mappedBy = "following", fetch = FetchType.EAGER)
+    public List<Users> followers;
+
     public void setPosts(List<Post> posts) {
         this.posts = posts;
     }
-
     public String getLocation() {
         return location;
     }
-
     public void setLocation(String location) {
         this.location = location;
     }
-
     public String getDateOfBirth() {
         return dateOfBirth;
     }
-
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
-
     public Users(String firstname, String lastname,String username, String password  , String location,String dateOfBirth, String bio) {
         this.firstname = firstname;
         this.lastname = lastname;
@@ -59,91 +70,73 @@ public class Users implements UserDetails {
         this.username = username;
         this.password = password;
     }
-
     public String getBio() {
         return bio;
     }
-
     public void setBio(String bio) {
         this.bio = bio;
     }
-
-
-
     @Column(unique = true)
     String username;
-
     String password;
-
     public void addPost(Post body){
         this.posts.add(body);
-
     }
-
-
     public Long getId() {
         return id;
     }
-
     public String getFirstname() {
         return firstname;
     }
-
     public void setFirstname(String firstName) {
         this.firstname = firstName;
     }
-
     public String getLastname() {
         return lastname;
     }
-
     public void setLastname(String lastName) {
         this.lastname = lastName;
     }
-
-
     public Users() {
-
     }
-
     public Users(String username, String password) {
         this.username = username;
         this.password = password;
-
     }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
-
     @Override
     public String getPassword() {
         return password;
     }
-
     @Override
     public String getUsername() {
         return username;
     }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
     @Override
     public boolean isEnabled() {
         return true;
+    }
+    public Set<Users> addFollowing(Users users){
+        this.following.add(users);
+        return this.following ;
+    }
+    public Set<Users> deleteFollowing(Users userAccount){
+        this.following.remove(userAccount);
+        return this.following ;
     }
 }
